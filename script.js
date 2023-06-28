9df01959caf2a404c10df4b4be24a2a6524d31e4
Bambi <ianvinuya7@gmail.com>
2023-06-28 12:44:21 +0800

    Improve nav and hero section

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ const navhum = document.querySelectorAll(".navhum");
 toggle.addEventListener("click", () => {
   navlong.classList.toggle("long");
   navhum.forEach((item) => item.classList.toggle("long"));
-
+  navhum.forEach((item) => item.classList.toggle("nav"));
   if (navbar.classList.contains("bottom-20")) {
     navbar.classList.remove("bottom-20");
-
+    navbar.classList.add("shadow-2xl");
     navbar.classList.add("-bottom-72");
   } else if (navbar.classList.contains("-bottom-72")) {
     navbar.classList.remove("-bottom-72");
+    navbar.classList.remove("shadow-2xl");
     navbar.classList.add("bottom-20");
   }
 });
